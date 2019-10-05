6945f026d475bdd8f105ed338aecb556b3e230d9
kongnyc <54258089+kongnyc@users.noreply.github.com>
2019-10-05 12:08:43 -0400

    problem 1 to 4

---
+++ stringlab.js
@@ -0,0 +1,11 @@
+const partA = "David the best"
+console.log(partA[0])
+
+console.log(partA.length)
+
+console.log(partA[partA.length-1])
+
+
+// console.log(partA.toUpperCase([partA.length-1]))
+console.log(partA.slice(0,partA.length-1)+ partA[partA.length-1].toUpperCase())
+
