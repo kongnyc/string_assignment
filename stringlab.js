6162b99eb174d71a28cdf953cf61e7acd663d247
david maravillas <davidmaravillas@pursuit.org>
2019-10-05 13:59:41 -0400

    COMPLETE Q 5-7

--- stringlab.js
+++ stringlab.js
@@ -9,3 +9,31 @@ console.log(partA[partA.length-1])
 // console.log(partA.toUpperCase([partA.length-1]))
 console.log(partA.slice(0,partA.length-1)+ partA[partA.length-1].toUpperCase())
 
+// 5
+let amount = 90000000
+
+if ( amount >= 1000000){
+    console.log(`${amount} dollars (pinky)`)
+    } else {
+        console.log(`${amount} dollars`)
+    }
+
+// 6 
+
+let verb = "too" ;
+
+if( verb.length > 3 && verb.slice(verb.length-3,verb.length) === 'ing'){
+    console.log(verb + "ly")
+} else if (verb.length > 3 ){
+    console.log(verb + 'ing')
+} else{
+    console.log(verb)
+}
+
+// 7
+
+let strSev = 'withoutLast'
+const lost = 'last'
+
+console.log(strSev.slice(0,strSev.length-lost.length))
+
